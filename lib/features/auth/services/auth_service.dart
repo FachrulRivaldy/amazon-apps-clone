cf731fe43b773d8f96628f593344cba4128943ce
Fachrul Rivaldy <fachrul.rivaldy13@gmail.com>
2022-07-13 17:31:04 +0700

    Setup Client Sign Up and Error Handling

---
+++ lib/features/auth/services/auth_service.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:amazon_apps_clone/constants/global_variables.dart';
+import 'package:amazon_apps_clone/models/user.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  //SIGN UP USER
+  void signUpUser({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      User user = User(
+        id: '',
+        name: name,
+        email: email,
+        password: password,
+        address: '',
+        type: '',
+        token: '',
+      );
+
+      http.Response res = await http.post(Uri.parse('$uri/api/signup'),
+          body: user.toJson(),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8'
+          });
+    } catch (e) {}
+  }
+}
